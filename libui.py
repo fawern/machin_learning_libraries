dee0958112dd84449e62248fede7b2a04605117f
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-02-06 16:58:46 +0300

    model df added to ui

--- libui.py
+++ libui.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk 
 
+import customtkinter
+import CTkTable 
+
 import classification
 import regression
 
@@ -21,7 +24,7 @@ def upload_button_click():
 
 def train_button_click(selected_model):
     if selected_model == 'Classification':
-        pass
+        print(selected_model)
     
     elif selected_model == 'Regression':
         pass
@@ -29,6 +32,9 @@ def train_button_click(selected_model):
     else:
         messagebox.showinfo(title='Not found error', message="Model type not selected")
     return None
+
+# def change_theme():
+#     customtkinter.set_appearance_mode("light")
 
 def on_close_click():
     root.destroy()
@@ -38,22 +44,27 @@ def library_ui():
     global root 
     global model_type
 
-    colour1 = '#020f12' # Background color
+    # colour1 = '#292929' # Background color
+    colour1 = '#161616'
     colour2 = '#05d7ff' # Default button background color
     colour3 = '#246C71' # Button background color when clicked
     colour4 = 'black' # Text color
 
-    root = tk.Tk()
+    root = customtkinter.CTk()
+    
     root.title("Machine Learnings Library")
     root.resizable(True, True)
-    root.geometry("800x600")  # Width x Height
+    root.geometry("1000x600")  # Width x Height
     root.configure(background=colour1)
+    
+    left_container = customtkinter.CTkFrame(root, corner_radius=15)
+    left_container.place(relx=0.03, rely=0.05, relwidth=0.3, relheight=0.9)
 
-    label = tk.Label(root, text="Add a CSV file for models")
+    label = tk.Label(left_container, text="Add a CSV file for models")
     label.pack(pady=10)
 
     upload_button = tk.Button(
-        root,
+        left_container,
         text='Upload',
         command=upload_button_click,
         background=colour2, # Background color when not hovered
@@ -67,19 +78,23 @@ def library_ui():
         height=1,
         border=0,
         cursor='hand1',
-        font=('Arial', 16, 'bold')
+        font=('Arial', 16, 'bold'),
     )
     upload_button.pack(pady=10)
 
-    n = tk.StringVar()
-    model_type = ttk.Combobox(root, width=12, textvariable=n)
-    model_type['values'] = ('Classification', 'Regression')
-    model_type.pack(pady=10) 
+    combobox_var = customtkinter.StringVar(value="Machine Learning Type")
+    combobox = customtkinter.CTkComboBox(
+        master=left_container,
+        width=170,     
+        values=['Classification', 'Regression'],
+        variable=combobox_var
+    )
+    combobox.pack(padx=20, pady=10)
 
     train_button = tk.Button(
-        root,
+        left_container,
         text='Train Models',
-        command=lambda : train_button_click(model_type.get()),
+        command=lambda : train_button_click(combobox.get()),
         background=colour2, # Background color when not hovered
         foreground=colour4, # Text color
         activebackground=colour3, # Background color when hovered/clicked
@@ -96,7 +111,7 @@ def library_ui():
     train_button.pack(pady=10)
 
     close_button = tk.Button(
-        root,
+        left_container,
         text='Close',
         command=on_close_click,
         background=colour3,
@@ -113,6 +128,18 @@ def library_ui():
         font=('Arial', 16, 'bold')
     )
     close_button.pack(side='bottom', pady=50)
+
+    value = [[1,2,3,4,5],
+            [1,2,3,4,5],
+            [1,2,3,4,5],
+            [1,2,3,4,5],
+            [1,2,3,4,5]]
+
+    table_container = customtkinter.CTkFrame(root, corner_radius=15)
+    table_container.place(relx=0.35, rely=0.05, relwidth=0.6, relheight=0.9)
+
+    table = CTkTable.CTkTable(master=table_container, row=5, column=5, values=value, header_color=colour1)
+    table.pack(expand=True, fill="both", padx=20, pady=20)
 
     root.mainloop()
     
