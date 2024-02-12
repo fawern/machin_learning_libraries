1d2cc5d4a3177786ded7785ee96d30d5b07b5c5c
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-02-12 13:45:53 +0300

    function that show models output to ui added

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Machine Learning Algorithms
 
-In the classification.py and regression.py files, you can find the implementations of various machine learning algorithms. 
-These algorithms can automatically search for the best hyperparameters and outputs of all algorithms when called from the main.ipynb file. 
-By providing the test and training datasets, you don't need to write all the algorithms individually to find the most suitable one for your dataset. 
+In the classification.py and regression.py files, you can find the implementations of various machine learning algorithms.
+These algorithms can automatically search for the best hyperparameters and outputs of all algorithms when called from the main.ipynb file.
+By providing the test and training datasets, you don't need to write all the algorithms individually to find the most suitable one for your dataset.
 This significantly simplifies the process of finding the optimal algorithm for your specific dataset.
+
+![before](./Screenshot%202024-02-12%20134137.png)
+
+![after](./Screenshot%202024-02-12%20134157.png)
--- libui.py
+++ libui.py
@@ -5,33 +5,65 @@ from tkinter import ttk
 import customtkinter
 import CTkTable 
 
-import classification
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+import classification as cls_
 import regression
 
 file_path = ''
-
 def upload_button_click():
     global file_path
 
     file_path = filedialog.askopenfilename(title='Select a CSV file !!!')
-    label.config(text=file_path.split('/')[-1])
-
     if file_path:
         if file_path.split('.')[-1] != 'csv':
             messagebox.showinfo(title='File type error', message="Please select a CSV file")
     else:
         messagebox.showinfo(title='Not found error', message="No file selected")
 
+
+models_output_list = []
 def train_button_click(selected_model):
-    if selected_model == 'Classification':
-        print(selected_model)
+    global models_output_list
     
+    if selected_model == 'Classification':
+        df = pd.read_csv(file_path)
+        X = df.iloc[:, :-1].values
+        y = df.iloc[:, -1]
+
+        y = y.map({'Onaylandı': 1, 'Reddedildi': 0})
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=.2, random_state=42
+        )       
+
+        models_output_list = cls_.classification_models(X_train, X_test, y_train, y_test)[0]
+
+        print(models_output_list)
+
+        show_model_output(models_output_list)
+
     elif selected_model == 'Regression':
-        pass
+        print(selected_model)
 
     else:
         messagebox.showinfo(title='Not found error', message="Model type not selected")
+
     return None
+
+def show_model_output(output_df):
+    value = [list(models_output_list.columns)]
+
+    for row in models_output_list.values:
+        value.append(list(row))
+
+    table_container = customtkinter.CTkFrame(root, corner_radius=15)
+    table_container.place(relx=0.3, rely=0.05, relwidth=0.68, relheight=0.9)
+
+    table = CTkTable.CTkTable(
+        master=table_container, row=len(output_df) + 1, column=len(output_df.columns), 
+        values=value, header_color="#204261")
+    table.pack(expand=True, fill="both", padx=20, pady=20)
 
 # def change_theme():
 #     customtkinter.set_appearance_mode("light")
@@ -43,6 +75,7 @@ def library_ui():
     global label
     global root 
     global model_type
+    global colour1
 
     # colour1 = '#292929' # Background color
     colour1 = '#161616'
@@ -54,27 +87,27 @@ def library_ui():
     
     root.title("Machine Learnings Library")
     root.resizable(True, True)
-    root.geometry("1000x600")  # Width x Height
+    root.geometry("1400x600")  # Width x Height
     root.configure(background=colour1)
     
     left_container = customtkinter.CTkFrame(root, corner_radius=15)
-    left_container.place(relx=0.03, rely=0.05, relwidth=0.3, relheight=0.9)
+    left_container.place(relx=0.025, rely=0.05, relwidth=0.25, relheight=0.9)
 
-    label = tk.Label(left_container, text="Add a CSV file for models")
-    label.pack(pady=10)
+    # label = tk.Label(left_container, text="Add a CSV file for models")
+    # label.pack(pady=10)
 
     upload_button = tk.Button(
         left_container,
-        text='Upload',
+        text='Upload .csv',
         command=upload_button_click,
         background=colour2, # Background color when not hovered
         foreground=colour4, # Text color
         activebackground=colour3, # Background color when hovered/clicked
         activeforeground=colour4, # Text color when hovered/clicked
         highlightthickness=2, # Thickness of the highlight border
-        highlightbackground=colour2, # Highlight border color
+        highlightbackground=colour2, # Highlight border color2
         highlightcolor='WHITE', # Highlight color
-        width=8,
+        width=9,
         height=1,
         border=0,
         cursor='hand1',
@@ -129,17 +162,6 @@ def library_ui():
     )
     close_button.pack(side='bottom', pady=50)
 
-    value = [[1,2,3,4,5],
-            [1,2,3,4,5],
-            [1,2,3,4,5],
-            [1,2,3,4,5],
-            [1,2,3,4,5]]
-
-    table_container = customtkinter.CTkFrame(root, corner_radius=15)
-    table_container.place(relx=0.35, rely=0.05, relwidth=0.6, relheight=0.9)
-
-    table = CTkTable.CTkTable(master=table_container, row=5, column=5, values=value, header_color=colour1)
-    table.pack(expand=True, fill="both", padx=20, pady=20)
 
     root.mainloop()
     
