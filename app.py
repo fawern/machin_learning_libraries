b8707e755f2b30842d8db934680840bacbe7c264
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-02-16 17:34:46 +0300

    app file updated-clear button removed

---
+++ app.py
@@ -0,0 +1,5 @@
+from libui import library_ui
+
+if __name__ == "__main__":
+    library_ui()
+    
--- libui.py
+++ libui.py
@@ -1,16 +1,18 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from tkinter import ttk 
+from tkinter import ttk
 
 import customtkinter
-import CTkTable 
+import CTkTable
 
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 import classification as cls_
 import regression
 
-df_columns = []
+global colour1
+
+global table
 
 def upload_button_click():
     global file_path
@@ -21,18 +23,17 @@ def upload_button_click():
     if file_path:
         if file_path.split('.')[-1] != 'csv':
             messagebox.showinfo(title='File type error', message="Please select a CSV file")
-            return
-
-        df_columns = list(pd.read_csv(file_path).columns)
-        target_combobox.configure(values=df_columns)
+        else:
+            df_columns = list(pd.read_csv(file_path).columns)
+            target_combobox.configure(values=df_columns)
     else:
         messagebox.showinfo(title='Not found error', message="No file selected")
 
 
 
-models_output_list = []
 def train_button_click(selected_model):
     global models_output_list
+
     target_col = target_combobox.get()
 
     if selected_model == 'Classification':
@@ -67,9 +68,14 @@ def train_button_click(selected_model):
     return None
 
 
-def show_model_output():
-    global table
+def truncate_number(number):
+    places = 3
+    multipler = 10 ** places
+    number = int(number * multipler) / multipler
+    
+    return number
 
+def show_model_output():
     value = [list(models_output_list.columns)]
 
     for row in models_output_list.values:
@@ -86,41 +92,21 @@ def show_model_output():
 # def change_theme():
 #     customtkinter.set_appearance_mode("light")
 
-
-def on_clear_click():
-    global file_path
-    # Output df table
-    table_container = customtkinter.CTkFrame(root, corner_radius=15)
-    table_container.place(relx=0.3, rely=0.05, relwidth=0.68, relheight=0.9)
-
-    table = CTkTable.CTkTable(master=table_container)
-    # ----
-
-    file_path = ''
-    target_combobox.configure(values=[''])
-    combobox.configure(values=[''])
-
 def on_close_click():
     root.destroy()
 
 def library_ui():
-    global label
     global root
-    global model_type
-    global colour1
     global left_container
     global target_combobox
-    global output_col_var
-    global combobox
+    
 
-    # colour1 = '#292929' # Background color
     colour1 = '#161616'
     colour2 = '#05d7ff' # Default button background color
     colour3 = '#246C71' # Button background color when clicked
     colour4 = 'black' # Text color
 
     root = customtkinter.CTk()
-
     root.title("Machine Learnings Library")
     root.resizable(True, True)
     root.geometry("1400x600")  # Width x Height
@@ -128,9 +114,6 @@ def library_ui():
 
     left_container = customtkinter.CTkFrame(root, corner_radius=15)
     left_container.place(relx=0.025, rely=0.05, relwidth=0.25, relheight=0.9)
-
-    # label = tk.Label(left_container, text="Add a CSV file for models")
-    # label.pack(pady=10)
 
     upload_button = tk.Button(
         left_container,
@@ -189,27 +172,6 @@ def library_ui():
     )
     train_button.pack(pady=10)
 
-
-    clear_button = tk.Button(
-        left_container,
-        text='Clear',
-        command=on_clear_click,
-        background=colour3,
-        foreground=colour2,
-        activebackground=colour3,
-        activeforeground=colour4,
-        highlightthickness=2,
-        highlightbackground=colour2,
-        highlightcolor='WHITE',
-        width=8,
-        height=1,
-        border=0,
-        cursor='hand1',
-        font=('Arial', 16, 'bold')
-    )
-    clear_button.pack(pady=50)
-
-
     close_button = tk.Button(
         left_container,
         text='Close',
@@ -230,7 +192,3 @@ def library_ui():
     close_button.pack(side='bottom', pady=50)
 
     root.mainloop()
-    
-    return file_path
-
-library_ui()
