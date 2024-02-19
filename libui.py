5abaf5645af095be4b5a7e98938381c06bcc3636
Muhammed Enes Ekinci <211805030@stu.adu.edu.tr>
2024-02-19 12:06:49 +0300

    Library_UI added

--- libui.py
+++ libui.py
@@ -1,194 +1,181 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
-from tkinter import ttk
-
 import customtkinter
+from tkinter import filedialog, messagebox
 import CTkTable
 
-import pandas as pd
+import pandas as pd 
 from sklearn.model_selection import train_test_split
 import classification as cls_
 import regression
 
-global colour1
+class Library_GUI:
+    def __init__(self):
+        self.colour1 = '#161616'
+        self.colour2 = '#05d7ff' # Default button background color
+        self.colour3 = '#246C71' # Button background color when clicked
+        self.colour4 = 'black' # Text color
 
-global table
+        self.root = None
+        self.left_container = None
+        self.target_combobox = None
 
-def upload_button_click():
-    global file_path
-    global df_columns
+        # Upload file button
+        self.file_path = None
+        self.df_columns = None
+        self.target_combobox = None
 
-    file_path = filedialog.askopenfilename(title='Select a CSV file !!!')
+        # Train model button
+        self.models_output_list = None
 
-    if file_path:
-        if file_path.split('.')[-1] != 'csv':
-            messagebox.showinfo(title='File type error', message="Please select a CSV file")
+    def upload_button_click(self):
+        self.file_path = filedialog.askopenfilename(title='Select a CSV file !!!')
+
+        if self.file_path:
+            if self.file_path.split('.')[-1] != 'csv':
+                messagebox.showinfo(title='File type error', message="Please select a CSV file")
+            else:
+                self.df_columns = list(pd.read_csv(self.file_path).columns)
+                self.target_combobox.configure(values=self.df_columns)
         else:
-            df_columns = list(pd.read_csv(file_path).columns)
-            target_combobox.configure(values=df_columns)
-    else:
-        messagebox.showinfo(title='Not found error', message="No file selected")
+            messagebox.showinfo(title='Not found error', message="No file selected")
 
+    def train_button_click(self, selected_model):
+        target_col = self.target_combobox.get()
 
+        if selected_model == 'Classification':
+            df = pd.read_csv(self.file_path)
+            X = df.drop(columns=[target_col]).values
+            y = df[target_col]
 
-def train_button_click(selected_model):
-    global models_output_list
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=.2, random_state=42
+            )       
 
-    target_col = target_combobox.get()
+            self.models_output_list = cls_.classification_models(X_train, X_test, y_train, y_test)[0]
 
-    if selected_model == 'Classification':
-        df = pd.read_csv(file_path)
-        X = df.drop(columns=[target_col]).values
-        y = df[target_col]
+            self.show_model_output()
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=.2, random_state=42
-        )       
+        elif selected_model == 'Regression':
+            df = pd.read_csv(self.file_path)
+            X = df.drop(columns=[target_col]).values
+            y = df[target_col]
 
-        models_output_list = cls_.classification_models(X_train, X_test, y_train, y_test)[0]
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=.2, random_state=42
+            )       
 
-        show_model_output()
+            models_output_list = regression.regression_models(X_train, X_test, y_train, y_test)[0]
 
-    elif selected_model == 'Regression':
-        df = pd.read_csv(file_path)
-        X = df.drop(columns=[target_col]).values
-        y = df[target_col]
+            self.show_model_output()
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=.2, random_state=42
-        )       
+        else:
+            messagebox.showinfo(title='Not found error', message="Model type not selected")
 
-        models_output_list = regression.regression_models(X_train, X_test, y_train, y_test)[0]
+        return None
 
-        show_model_output()
+    def show_model_output(self):
+        value = [list(self.models_output_list.columns)]
 
-    else:
-        messagebox.showinfo(title='Not found error', message="Model type not selected")
+        for row in self.models_output_list.values:
+            value.append(list(row))
 
-    return None
+        table_container = customtkinter.CTkFrame(self.root, corner_radius=15)
+        table_container.place(relx=0.3, rely=0.05, relwidth=0.68, relheight=0.9)
 
+        table = CTkTable.CTkTable(
+            master=table_container, row=len(self.models_output_list) + 1, column=len(self.models_output_list.columns), 
+            values=value, header_color="#204261")
+        table.pack(expand=True, fill="both", padx=20, pady=20)
 
-def truncate_number(number):
-    places = 3
-    multipler = 10 ** places
-    number = int(number * multipler) / multipler
-    
-    return number
+    def on_close_click(self):
+        self.root.destroy()
 
-def show_model_output():
-    value = [list(models_output_list.columns)]
+    def library_ui(self):    
+        self.root = customtkinter.CTk()
+        self.root.title("Machine Learnings Library")
+        self.root.resizable(True, True) 
+        self.root.geometry("1400x600")  # Width x Height
+        self.root.configure(background=self.colour1)
 
-    for row in models_output_list.values:
-        value.append(list(row))
+        self.left_container = customtkinter.CTkFrame(self.root, corner_radius=15)
+        self.left_container.place(relx=0.025, rely=0.05, relwidth=0.25, relheight=0.9)
 
-    table_container = customtkinter.CTkFrame(root, corner_radius=15)
-    table_container.place(relx=0.3, rely=0.05, relwidth=0.68, relheight=0.9)
+        upload_button = tk.Button(
+            self.left_container,
+            text = 'Upload .csv',
+            command = self.upload_button_click,
+            background = self.colour2, # Background color when not hovered
+            foreground = self.colour4, # Text color
+            activebackground = self.colour3, # Background color when hovered/clicked
+            activeforeground = self.colour4, # Text color when hovered/clicked
+            highlightthickness = 2, # Thickness of the highlight border
+            highlightbackground = self.colour2, # Highlight border color2
+            highlightcolor = 'WHITE', # Highlight color
+            width = 9,
+            height = 1,
+            border = 0,
+            cursor = 'hand1',
+            font = ('Arial', 16, 'bold')
+        )
+        upload_button.pack(pady=10)
 
-    table = CTkTable.CTkTable(
-        master=table_container, row=len(models_output_list) + 1, column=len(models_output_list.columns), 
-        values=value, header_color="#204261")
-    table.pack(expand=True, fill="both", padx=20, pady=20)
+        output_col_var = customtkinter.StringVar(value="Target Column")
+        self.target_combobox = customtkinter.CTkComboBox(
+            master = self.left_container,
+            width = 170,
+            variable = output_col_var,
+            values = ['']
+        )
+        self.target_combobox.pack(padx=20, pady=10)
 
-# def change_theme():
-#     customtkinter.set_appearance_mode("light")
+        model_type_var = customtkinter.StringVar(value="Machine Learning Type")
 
-def on_close_click():
-    root.destroy()
+        combobox = customtkinter.CTkComboBox(
+            master = self.left_container,
+            width = 170,     
+            values = ['Classification', 'Regression'],
+            variable  =model_type_var
+        )
+        combobox.pack(padx=20, pady=10)
 
-def library_ui():
-    global root
-    global left_container
-    global target_combobox
-    
+        train_button = tk.Button(
+            self.left_container,
+            text = 'Train Models',
+            command = lambda : self.train_button_click(combobox.get()),
+            background = self.colour2, # Background color when not hovered
+            foreground = self.colour4, # Text color
+            activebackground = self.colour3, # Background color when hovered/clicked
+            activeforeground = self.colour4, # Text color when hovered/clicked
+            highlightthickness = 2, # Thickness of the highlight border
+            highlightbackground = self.colour2, # Highlight border color
+            highlightcolor = 'WHITE', # Highlight color
+            width = 10,
+            height = 1,
+            border = 0,
+            cursor = 'hand1',
+            font = ('Arial', 16, 'bold')
+        )
+        train_button.pack(pady=10)
 
-    colour1 = '#161616'
-    colour2 = '#05d7ff' # Default button background color
-    colour3 = '#246C71' # Button background color when clicked
-    colour4 = 'black' # Text color
+        close_button = tk.Button(
+            self.left_container,
+            text = 'Close',
+            command = self.on_close_click,
+            background = '#a4072c', # gozuken renk
+            foreground = 'white',
+            activebackground = '#94042b', # tiklayinca 
+            activeforeground = self.colour4,
+            highlightthickness = 2,
+            highlightbackground = self.colour2,
+            highlightcolor = 'WHITE',
+            width = 8,
+            height = 1,
+            border = 0,
+            cursor = 'hand1',
+            font = ('Arial', 16, 'bold')
+        )
+        close_button.pack(side='bottom', pady=50)
 
-    root = customtkinter.CTk()
-    root.title("Machine Learnings Library")
-    root.resizable(True, True)
-    root.geometry("1400x600")  # Width x Height
-    root.configure(background=colour1)
+        self.root.mainloop()
 
-    left_container = customtkinter.CTkFrame(root, corner_radius=15)
-    left_container.place(relx=0.025, rely=0.05, relwidth=0.25, relheight=0.9)
-
-    upload_button = tk.Button(
-        left_container,
-        text='Upload .csv',
-        command=upload_button_click,
-        background=colour2, # Background color when not hovered
-        foreground=colour4, # Text color
-        activebackground=colour3, # Background color when hovered/clicked
-        activeforeground=colour4, # Text color when hovered/clicked
-        highlightthickness=2, # Thickness of the highlight border
-        highlightbackground=colour2, # Highlight border color2
-        highlightcolor='WHITE', # Highlight color
-        width=9,
-        height=1,
-        border=0,
-        cursor='hand1',
-        font=('Arial', 16, 'bold')
-    )
-    upload_button.pack(pady=10)
-
-    output_col_var = customtkinter.StringVar(value="Target Column")
-    target_combobox = customtkinter.CTkComboBox(
-        master=left_container,
-        width=170,
-        variable=output_col_var,
-        values=['']
-    )
-    target_combobox.pack(padx=20, pady=10)
-
-    model_type_var = customtkinter.StringVar(value="Machine Learning Type")
-
-    combobox = customtkinter.CTkComboBox(
-        master=left_container,
-        width=170,     
-        values=['Classification', 'Regression'],
-        variable=model_type_var
-    )
-    combobox.pack(padx=20, pady=10)
-
-    train_button = tk.Button(
-        left_container,
-        text='Train Models',
-        command=lambda : train_button_click(combobox.get()),
-        background=colour2, # Background color when not hovered
-        foreground=colour4, # Text color
-        activebackground=colour3, # Background color when hovered/clicked
-        activeforeground=colour4, # Text color when hovered/clicked
-        highlightthickness=2, # Thickness of the highlight border
-        highlightbackground=colour2, # Highlight border color
-        highlightcolor='WHITE', # Highlight color
-        width=10,
-        height=1,
-        border=0,
-        cursor='hand1',
-        font=('Arial', 16, 'bold')
-    )
-    train_button.pack(pady=10)
-
-    close_button = tk.Button(
-        left_container,
-        text='Close',
-        command=on_close_click,
-        background='#a4072c', # gozuken renk
-        foreground='white',
-        activebackground='#94042b', # tiklayinca 
-        activeforeground=colour4,
-        highlightthickness=2,
-        highlightbackground=colour2,
-        highlightcolor='WHITE',
-        width=8,
-        height=1,
-        border=0,
-        cursor='hand1',
-        font=('Arial', 16, 'bold')
-    )
-    close_button.pack(side='bottom', pady=50)
-
-    root.mainloop()
+Library_GUI().library_ui()
